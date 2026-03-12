df2039a24ef580b9cc61cca1938fe0f8010ef1d9
Morgan <morgancampbell0425@gmail.com>
2026-03-13 08:29:58 +0900

    Add grid-item declaration block and update createGrid function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const container = document.querySelector('#container');
 
 function createGrid(size) {
+    container.textContent = '';
 
+    const totalSquares = size * size;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-item');
+        container.appendChild(square);
+    }
 }
