242e6c05b88dba8396f012f15c027d8d0a0e85b0
Morgan <morgancampbell0425@gmail.com>
2026-03-14 00:37:17 +0900

    Allow dynamic grid size and enable drawing by dragging mouse

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ function createGrid(size) {
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-item');
+
+        const squareSize = `${100 / size}%`;
+        square.style.width = squareSize;
+        square.style.height = squareSize;
+
         container.appendChild(square);
     }
 }
 
-createGrid(16);
+createGrid(128);
 
-container.addEventListener('mouseenter', function(event) {
-    if (event.target.classList.contains('grid-item')) {
+container.addEventListener('mouseover', function(event) {
+    if (event.target.classList.contains('grid-item') && event.buttons === 1) {
         event.target.classList.add('colored');
     }
-}, true);
+});
